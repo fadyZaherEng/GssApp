b550c8d44ed4cf1abb6a28bfc36a8e28db057889
sefeen <faragsefen2@gmail.com>
2023-09-10 16:30:22 +0300

    add comments

--- lib/presentation/blocs/sign_in/sign_in_events.dart
+++ lib/presentation/blocs/sign_in/sign_in_events.dart
@@ -1,8 +1,10 @@
 abstract class LogInEvents{}
-
+///change to SignInEvent
 class AppLogInEvent extends LogInEvents{}
 
+///change to SignInValidatePhoneNumberEvent
 class ValidatePhoneEventsSignIn extends LogInEvents{
+  /// change to validationMassage
   String val;
 
   ValidatePhoneEventsSignIn({required this.val});
--- lib/presentation/blocs/sign_in/sign_in_states.dart
+++ lib/presentation/blocs/sign_in/sign_in_states.dart
@@ -1,12 +1,19 @@
-abstract class LogInStates{}
+abstract class LogInStates {}
 
-class InitialLogINStates extends LogInStates{}
-class LoadingLogINStates extends LogInStates{}
-class SuccessLogINStates extends LogInStates{}
-class ErrorLogINStates extends LogInStates{}
+///Change to SignInInitialState
+class InitialLogINStates extends LogInStates {}
 
+/// Change to SignInLoadingState
+class LoadingLogINStates extends LogInStates {}
 
-class ValidatePhoneLoginStates extends LogInStates{
+///Change to SignInSuccessState
+class SuccessLogINStates extends LogInStates {}
+
+///Change to SignInErrorState
+class ErrorLogINStates extends LogInStates {}
+
+///Change to SignInValidatePhoneNumberState
+class ValidatePhoneLoginStates extends LogInStates {
   String? res;
 
   ValidatePhoneLoginStates({required this.res});
